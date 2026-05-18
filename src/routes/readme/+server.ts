942db319407c70986b5352bf419adb6cb4c198f2
Bart Oostveen <bart@bartoostveen.nl>
2026-05-18 18:48:25 +0200

    chore: cache readme stats heavily

--- src/routes/readme/+server.ts
+++ src/routes/readme/+server.ts
@@ -1,13 +1,23 @@
 import { error, type RequestHandler } from "@sveltejs/kit";
 import { env } from "$env/dynamic/private";
 
+let lastFetched = 0;
+let response: Blob | null = null;
+const dt = 30 * 60 * 1000;
+
 export const GET: RequestHandler = async () => {
   const endpoint = env.GH_README_STATS_ENDPOINT;
   if (!endpoint) {
     error(500, "Server is not capable of generating readme");
   }
+  if (response && (lastFetched + dt) <= Date.now()) {
+    return new Response(response);
+  }
   try {
-    return new Response(await (await fetch(endpoint)).blob());
+    const newResponse = await (await fetch(endpoint)).blob();
+    lastFetched = Date.now();
+    response = newResponse;
+    return new Response(newResponse);
   } catch (e) {
     console.error("Failed to generate readme", e);
     error(500, "Server cannot generate readme at this moment");
