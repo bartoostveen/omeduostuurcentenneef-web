7261e4ef91e85c4dfd17b754d88986f530dd061c
Bart Oostveen <bart@bartoostveen.nl>
2026-07-29 00:34:59 +0200

    chore: migrate to pnpm
    Closes #43

--- flake.nix
+++ flake.nix
@@ -1,40 +1,22 @@
 {
-  description = "Generic devshell flake";
+  description = "Placeholder website";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    bun2nix = {
-      url = "github:nix-community/bun2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{ self, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
-        "x86_64-darwin"
       ];
 
       imports = [
@@ -42,49 +24,27 @@
       ];
 
       perSystem =
-        {
-          pkgs,
-          system,
-          ...
-        }:
+        { pkgs, ... }:
 
         let
-          deps = with pkgs; [
-            nodejs_24
-            bun
-          ];
+          packageVersion = (./package.json |> builtins.readFile |> builtins.fromJSON).version;
         in
         {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.bun2nix.overlays.default
-            ];
-          };
-
           treefmt = {
             programs.nixfmt.enable = true;
-            programs.deadnix = {
-              enable = true;
-              excludes = [ "bun.nix" ];
-            };
+            programs.deadnix.enable = true;
           };
 
           devShells.default = pkgs.mkShell {
-            packages = deps;
+            packages = [
+              pkgs.nodejs
+              pkgs.pnpm_11
+            ];
           };
 
-          packages.default = pkgs.callPackage ./package.nix { };
-
-          apps.bun2nix-update = {
-            type = "app";
-            program = pkgs.writeShellApplication {
-              name = "bun2nix-update";
-              runtimeInputs = deps;
-              text = ''
-                bun run bun2nix:update
-              '';
-            };
+          packages.default = (pkgs.callPackage ./package.nix { }).overrideAttrs {
+            version = "${packageVersion}-${self.shortRev or self.dirtyShortRev or "dirty-norev"}";
+            __intentionallyOverridingVersion = true;
           };
         };
     };
--- package.nix
+++ package.nix
@@ -1,36 +1,60 @@
 {
-  bun2nix,
   lib,
-  makeWrapper,
+  stdenv,
+  pnpm_11,
+  fetchPnpmDeps,
+  pnpmConfigHook,
   nodejs,
-  ...
+  makeWrapper,
 }:
 
 let
+  pnpm = pnpm_11;
+
   inherit (lib) getExe;
 in
-bun2nix.mkDerivation {
-  src = ./.;
-  packageJson = ./package.json;
+stdenv.mkDerivation (finalAttrs: {
+  pname = "omeduoweb";
+  version = "unstable";
 
-  bunDeps = bun2nix.fetchBunDeps {
-    bunNix = ./bun.nix;
-  };
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  src = ./.;
 
   nativeBuildInputs = [
+    nodejs
+    pnpm
+    pnpmConfigHook
     makeWrapper
   ];
 
+  pnpmDeps = fetchPnpmDeps {
+    inherit (finalAttrs) pname version src;
+    inherit pnpm;
+    fetcherVersion = 4;
+    hash = "sha256-qQLqKGfqw2WMGaza1tptPgn3SeZthQwNkKjop70jk7o=";
+  };
+
   buildPhase = ''
     runHook preBuild
-    bun run build
+    pnpm run build
     runHook postBuild
   '';
 
   installPhase = ''
     runHook preInstall
     cp -r build $out/
-    makeWrapper ${getExe nodejs} $out/bin/omeduostuurcentenneef-web --append-flag "$out"
+    makeWrapper ${getExe nodejs} $out/bin/omeduoweb --append-flag "$out"
     runHook postInstall
   '';
-}
+
+  meta = {
+    description = "";
+    homepage = "https://git.bartoostveen.nl/bart/omeduoweb";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ bartoostveen ];
+    mainProgram = "omeduoweb";
+    platforms = lib.platforms.all;
+  };
+})
