ef5c7ebc2646b0d82f84b9b880fa306632e3cc51
Bart Oostveen <bart@bartoostveen.nl>
2026-08-28 11:29:50 +0200

    chore(deps): pnpm upgrade

--- package.nix
+++ package.nix
@@ -33,7 +33,7 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs) pname version src;
     inherit pnpm;
     fetcherVersion = 4;
-    hash = "sha256-R1HFKVCN9lKdcM/63pmZNL1gt/9IYMYz5VNzQXkUulU=";
+    hash = "sha256-hFsU5mGzNmDJoTtlzhweyxTqn/JCkcbLQ369z3wR7QE=";
   };
 
   buildPhase = ''
